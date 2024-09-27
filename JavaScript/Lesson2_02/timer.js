4a32a06b1b0b52b0f8e9ac8be0728887accf29af
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-09-27 14:42:26 +0900

    updated Lesson 2_02

--- JavaScript/Lesson2_02/timer.js
+++ JavaScript/Lesson2_02/timer.js
@@ -1,21 +1,34 @@
 let finish
+let intervalId
+let startButton = document.querySelector("#start-button")
+startButton.addEventListener("click", startTimer)
+let stopButton = document.querySelector("#stop-button")
+stopButton.addEventListener("click", stopTimer)
 
 //start the timer
 function startTimer(){
-    let second = document.querySelector("#timer-input").value
+    let second = document.querySelector("#time-input").value
 
     finish = Date.now() + second * 1000
-    intervalId = setInterval(checkRemainingTime, 50);
+    intervalId = setInterval(checkRemainingTime, 50)
+
+    startButton.disabled = true
 }
 
 //stop the timer
 function stopTimer(){
+    clearInterval(intervalId)
+    setDisplay(0)
 
+    startButton.disabled = false
 }
 
 // loop checking remaining time
 function checkRemainingTime(){
     let remain = finish - Date.now()
+
+    let second = Math.floor(remain / 1000) + 1
+    setDisplay(second)
 
     if(remain <= 0){
         stopTimer()
@@ -24,6 +37,7 @@ function checkRemainingTime(){
 }
 
 //show remaining time
-function setDisplay(){
-
+function setDisplay(second){
+    let countDown = document.querySelector("#count-down")
+    countDown.textContent = second
 }
